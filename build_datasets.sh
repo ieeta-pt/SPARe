fdc836f76d0933e2db3cc28319a860b8f666fc6b
Tiago Almeida <tiagomeloalmeida@ua.pt>
2024-01-10 23:05:49 +0000

    clean up the code on master

--- build_datasets.sh
+++
@@ -1,8 +0,0 @@
-DATASET_PATH="beir_datasets"
-
-python download_beir_dataset.py $DATASET_PATH
-
-bash convert_from_anserini.sh
-
-bash convert_from_terrier.sh
-
--- run_benchmarks.sh
+++
@@ -1,4 +0,0 @@
-for f in beir_datasets/*
-do
-    python benchmark_pyterrier.py $f
-done
